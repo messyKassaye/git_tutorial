d5230e75866d401dfe1d040b840aee10545af04d
messykassayes <meseret.kassayes@gmail.com>
2019-12-01 12:24:35 +0300

    latest commit

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MainNavigation extends Component {
+    render() {
+        return (
+            <div>
+                Main Navigation
+            </div>
+        )
+    }
+}
